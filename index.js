593d59a3e788fda563fe76a477e49313f1517874
Kulik Oleg <kulik.ol.val@gmail.com>
2024-02-16 14:46:24 +0700

    test commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         'Content-Type': 'text/html'
     })
-    res.end("Hello Homework")
+    res.end("Hello Homework\n")
 })
 
 server.listen(port, ip, () => {
