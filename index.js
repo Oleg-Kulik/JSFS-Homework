4fef41a6f5b20d9c30aeffce9a1c535c79395cdb
Kulik Oleg <kulik.ol.val@gmail.com>
2024-02-16 13:39:53 +0700

    JSFS Homework is created

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+
+const ip = "127.0.0.1"
+const port = 3000
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    res.end("Hello Homework")
+})
+
+server.listen(port, ip, () => {
+    console.log(`Server is started. Port: ${port}, host: ${ip}`)
+})
